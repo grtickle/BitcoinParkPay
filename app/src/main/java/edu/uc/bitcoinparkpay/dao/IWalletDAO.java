dc96271c3bc0748c50d2f43432a9911f49efe043
ticklegr <ticklegr@mail.uc.edu>
2015-05-31 14:59:19 -0400

    Added IWalletDAO

---
+++ app/src/main/java/edu/uc/bitcoinparkpay/dao/IWalletDAO.java
@@ -0,0 +1,32 @@
+package edu.uc.bitcoinparkpay.dao;
+
+/**
+ * Created by Greg on 5/31/2015.
+ */
+public interface IWalletDAO {
+
+    /**
+     * Creates a wallet
+     * @param label the name of the wallet
+     * @throws Exception network problems
+     */
+    public void createWallet( String label ) throws Exception;
+
+    /**
+     * Gets balance of wallet
+     * @param label the name of the wallet
+     * @return
+     * @throws Exception network problems
+     */
+    public float getAddressBalance( String label ) throws Exception;
+
+    /**
+     * Sends bitcoin to another wallet address
+     * @param apiKey the account API Key
+     * @param amount of bitcoin to send
+     * @param to The wallet address to send to
+     * @param pin Account pin number
+     * @throws Exception network problems
+     */
+    public void send( String apiKey, float amount, String to, String pin ) throws Exception;
+}
