a2033c6da0dcdf0460753847521ed5fdc1557c53
ticklegr <ticklegr@mail.uc.edu>
2015-05-31 23:33:27 -0400

    Added IWalletDAO

---
+++ app/src/main/java/edu/uc/bitcoinparkpay/dao/NetworkDAO.java
@@ -0,0 +1,40 @@
+package edu.uc.bitcoinparkpay.dao;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Created by Greg on 5/31/2015.
+ */
+
+/**
+ * Low level networking stuff, can
+ * be used with several DAOs.
+ * @author jonesbr
+ * from https://github.uc.edu/jonesbr/15ssadvanced
+ */
+public class NetworkDAO {
+
+    public String fetch(String uri) throws Exception {
+
+        // Http Client marries together a request and response.
+        HttpClient httpClient = new DefaultHttpClient();
+
+        // what URI do we want to receive?
+        HttpGet httpGet = new HttpGet(uri);
+
+        // handle the response
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+        // access the URI and get back the return data.
+        String returnData = null;
+
+        returnData = httpClient.execute(httpGet, responseHandler);
+
+        // return data
+        return returnData;
+    }
+}
