df006454859f773e522ce6e4bb2ce57a5eda580e
ticklegr <ticklegr@mail.uc.edu>
2015-06-04 16:29:12 -0400

    Added WalletDAOStub

--- app/src/main/java/edu/uc/bitcoinparkpay/dao/WalletDAOStub.java
+++ app/src/main/java/edu/uc/bitcoinparkpay/dao/WalletDAOStub.java
@@ -6,9 +6,12 @@ import java.math.BigDecimal;
  * Created by Greg on 6/4/2015.
  */
 public class WalletDAOStub implements IWalletDAO{
+
     @Override
     public void createWallet(String label) throws Exception {
-
+        if ( label == null) {
+            throw new Exception("Error: No label given");
+        }
     }
 
     @Override
@@ -19,6 +22,10 @@ public class WalletDAOStub implements IWalletDAO{
 
     @Override
     public void send(String apiKey, BigDecimal amount, String to, String pin) throws Exception {
+
+        if ( apiKey == null || amount == null || to == null || pin == null) {
+            throw new Exception ("Error: missing parameter");
+        }
 
     }
 }
