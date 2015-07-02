f64535e618fc578210ceb382f081b0003d24e0c2
ticklegr <ticklegr@mail.uc.edu>
2015-07-01 20:17:21 -0400

    Changed constructor to call AddressDAO to create new address

--- app/src/main/java/edu/uc/bitcoinparkpay/dto/Address.java
+++ app/src/main/java/edu/uc/bitcoinparkpay/dto/Address.java
@@ -1,9 +1,6 @@
 package edu.uc.bitcoinparkpay.dto;
 
-import android.accounts.NetworkErrorException;
-import android.util.Log;
-
-import edu.uc.bitcoinparkpay.dao.NetworkDAO;
+import edu.uc.bitcoinparkpay.dao.AddressDAO;
 
 /**
  * Created by Greg on 6/29/2015.
@@ -13,33 +10,19 @@ public class Address {
 
     private String addressLabel;
 
-    NetworkDAO networkDAO;
+    AddressDAO addressDAO;
 
     public Address(String label) throws Exception{
         this.addressLabel = label;
 
-        networkDAO = new NetworkDAO();
+        //Create new wallet
+        addressDAO = new AddressDAO();
+        addressDAO.createWallet(label);
 
-        if ( label == null || label.isEmpty()) {
-            throw new Exception( "Error: No label given" );
-        } else {
-            try{
-                //*******Pull api keys from database*******
-                networkDAO.send( "https://block.io/api/v2/get_new_address/?api_key=3bb2-81fc-a60a-3e7c&label=" + label );
-            } catch ( NetworkErrorException e ){
-                Log.i("tag", "Error: No network connection.");
-            }
-        }
     }
 
     public String getAddressLabel() {
         return addressLabel;
     }
-
-    /**
-    public BigDecimal getBalance( String label) {
-        return null;
-    }
-     **/
 
 }
