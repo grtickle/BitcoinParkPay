bcc38c0b56b4a156937164cc9ea16a677b2799b7
ticklegr <ticklegr@mail.uc.edu>
2015-07-28 17:49:20 -0400

    Undo changes

--- app/src/main/java/edu/uc/bitcoinparkpay/CameraActivity.java
+++ app/src/main/java/edu/uc/bitcoinparkpay/CameraActivity.java
@@ -2,9 +2,7 @@ package edu.uc.bitcoinparkpay;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,16 +12,18 @@ import android.widget.ImageView;
 public class CameraActivity extends MainActivity {
     public static final int CAMERA_REQUEST = 10;
     private ImageView imgPhoto;
-
+/**
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         //associate the layout with this activity.
+        //Cannot resolve "activity_camera"
         setContentView(R.layout.activity_camera);
 
         //access image
+        //Cannot resolve "imgPhoto"
         imgPhoto = (ImageView) findViewById(R.id.imgPhoto);
     }
-
+**/
     //This method is called when the "Capture" button is clicked
     public void btnCaptureClicked(View v) {
         Intent CameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
