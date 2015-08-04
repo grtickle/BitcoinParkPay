83f83970aad380b2d36352f202c03ca4b22613bf
ticklegr <ticklegr@mail.uc.edu>
2015-08-03 20:52:12 -0400

    ScanData DTO

---
+++ app/src/main/java/edu/uc/bitcoinparkpay/dto/ScanData.java
@@ -0,0 +1,30 @@
+package edu.uc.bitcoinparkpay.dto;
+
+/**
+ * Created by Greg on 8/3/2015.
+ */
+public class ScanData {
+    private String address;
+    private double amount;
+
+    public ScanData(String address, double amount) {
+        this.address = address;
+        this.amount = amount;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
